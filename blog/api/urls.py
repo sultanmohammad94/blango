e2e34f2e0fcf015fd91f6706b5f20e40e7c98e07
MHD SULTAN <sultanmohammad94@gmail.com>
2022-12-01 18:19:43 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -25,11 +25,15 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
-
 ]
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("", include(router.urls)),
+        path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
     path("token-auth/", views.obtain_auth_token),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
